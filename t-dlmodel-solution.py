3ae73e8fe3c4e659e479a120637f4558f4c4c928
Romeo Kienzler <romeo.kienzler@gmail.com>
2020-01-07 14:32:43 +0100

    Add files via upload

---
+++ t-dlmodel-solution.py
@@ -0,0 +1,98 @@
+import gym
+import random
+import numpy as np
+from keras.models     import Sequential
+from keras.layers     import Dense
+from keras.optimizers import Adam
+
+# Let's create the Cart Pole OpenAI Gym game environment and define some constants
+env = gym.make('CartPole-v1')
+env.reset()
+goal_steps = 500
+score_requirement = 60
+intial_games = 10000
+
+def model_data_preparation():
+    training_data = []
+    accepted_scores = []
+    for game_index in range(intial_games):
+        score = 0
+        game_memory = []
+        previous_observation = []
+        for step_index in range(goal_steps):
+            action = random.randrange(0, 2)
+            observation, reward, done, info = env.step(action)
+            
+            if len(previous_observation) > 0:
+                game_memory.append([previous_observation, action])
+                
+            previous_observation = observation
+            score += reward
+            if done:
+                break
+            
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0, 1]
+                elif data[1] == 0:
+                    output = [1, 0]
+                training_data.append([data[0], output])
+        
+        env.reset()
+
+    print(accepted_scores)
+    
+    return training_data
+
+training_data = model_data_preparation()
+
+
+
+# Please create a binary classifier neural network and train it with *training_data*
+
+def build_model(input_size, output_size):
+    model = Sequential()
+    model.add(Dense(128, input_dim=input_size, activation='relu'))
+    model.add(Dense(52, activation='relu'))
+    model.add(Dense(output_size, activation='linear'))
+    model.compile(loss='mse', optimizer=Adam())
+    return model
+
+def train_model(training_data):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
+    y = np.array([i[1] for i in training_data]).reshape(-1, len(training_data[0][1]))
+    model = build_model(input_size=len(X[0]), output_size=len(y[0]))
+    
+    model.fit(X, y, epochs=10)
+    return model
+
+trained_model = train_model(training_data)
+
+
+scores = []
+choices = []
+for each_game in range(100):
+    score = 0
+    prev_obs = []
+    for step_index in range(goal_steps):
+        env.render()
+        if len(prev_obs)==0:
+            action = random.randrange(0,2)
+        else:
+            action = np.argmax(trained_model.predict(prev_obs.reshape(-1, len(prev_obs)))[0])
+        
+        choices.append(action)
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        score+=reward
+        if done:
+            break
+
+    env.reset()
+    scores.append(score)
+
+print(scores)
+print('Average Score:', sum(scores)/len(scores))
+print('choice 1:{}  choice 0:{}'.format(choices.count(1)/len(choices),choices.count(0)/len(choices)))
